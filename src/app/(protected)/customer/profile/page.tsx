1f5e6131d05e5f8cb6c6d5806284ae94caa18b3e
Santi Tabbach <santitabbach@Mac.fibertel.com.ar>
2025-07-09 18:49:51 -0300

    Add customer profile page

---
+++ src/app/(protected)/customer/profile/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Profile from '@/features/users/components/profile/Profile';
+
+export default function ProfilePage() {
+	return (
+		<div className="min-h-screen bg-gray-50">
+			<Profile />
+		</div>
+	);
+}
